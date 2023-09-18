74a3841ce34763615ccd77ce1b45f933675aeffb
donnyDonowitz <philipp@englx.de>
2023-09-18 19:31:24 +0200

    feat: :rocket: a3

--- latencies_v2.R
+++ latencies_v2.R
@@ -28,16 +28,49 @@ path_to_datasets = c(
 )
 
 data <- data.frame()
+stichprobengroesse <- 100000
 
 # Schleife zum Einlesen der Datensätze
 for (i in path_to_datasets) {
   
   # Lese den Datensatz ein
   dat <- read.table(i, header = FALSE)
+  # Labeln und Beschriften
   colnames(dat) <- c("ID", "Timestamp", "ProcessingTime")
   dat$Dataset <- i
-  # Füge den eingelesenen Datensatz dem großen Datensatz hinzu
-  data <- rbind(data, dat)
+  if (grepl("arm_clock", i, fixed = TRUE)) {
+    dat$Architecture <-"ARM"
+  } else {
+    dat$Architecture <- "x86"
+  }
+  if (grepl("axfinder", i, fixed = TRUE)) {
+    cat("axfinder\n")
+    dat$Category <-"axfinder"
+  } else if ( grepl("pricespread", i, fixed = TRUE)) {
+    cat("pricespread\n")
+    dat$Category <-"pricespread"
+  } else {
+    cat("countone\n")
+    dat$Category <-"countone"
+  }
+  
+  # sampling ohne Verlust der Extremwerte
+  extremwerte <- dat %>% filter(ProcessingTime > quantile(ProcessingTime, 0.95))
+  stichprobe <- dat %>% anti_join(extremwerte, by = "ID") %>% sample_n(stichprobengroesse, replace = TRUE)
+  
+  # zusammenfuegen
+  
+  data <- rbind(data, stichprobe)
   cat("Datensatz", i, "eingelsen.\n")
 }
 print(data)
+
+ggplot(data = data, aes(x = Timestamp, y = ProcessingTime, color = Category)) +
+  # geom_point() +
+  # geom_line() +
+  geom_boxplot() +
+  # geom_dotplot() +
+  # facet_wrap(~Category) +
+  facet_grid(~Architecture) +
+  scale_x_continuous(labels = scales::comma) +  # Formatierung der x-Achsenbeschriftung
+  scale_y_continuous(labels = scales::comma)  # Formatierung der y-Achsenbeschriftung
